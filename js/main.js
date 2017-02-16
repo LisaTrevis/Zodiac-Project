d79cb04ea34099326c78aa7e900214367d8d3c78
Lisa Trevis <l_trevis@hotmail.com>
2017-02-15 21:18:06 -0800

    Altered base code to loop through array

--- js/main.js
+++ js/main.js
@@ -1,54 +1,111 @@
-function signFinder(monthOptions, dayOptions) {
-	if(monthOptions == "January" && dayOptions >= 20) {
-		var signName = "Aquarius";
-	} else if (monthOptions == "February" && dayOptions <= 18) {
-		var signName = "Aquarius";
-	} else if (monthOptions == "February" && 18 < dayOptions <=28)	{
-		var signName = "Pisces";
-	} else if (monthOptions == "March" && dayOptions <= 20) {
-		var signName = "Pisces";
-	} else if (monthOptions == "March" && dayOptions >= 21) {
-		var	signName = "Aries";
-	} else if (monthOptions == "April" && dayOptions <= 19) {
-		var signName = "Aries";
-	} else if (monthOptions == "April" && 19 < dayOptions <= 30) {
-		var signName = "Taurus";
-	} else if (monthOptions == "May" && dayOptions <= 20) {
-		var signName = "Taurus";
-	} else if (monthOptions == "May" && dayOptions >= 21) {
-		var signName = "Gemini";
-	} else if (monthOptions == "June" && dayOptions <= 20) {
-		var signName = "Gemini";
-	} else if (monthOptions = "June" && 20 < dayOptions <= 30) {
-		var signName = "Cancer";
-	} else if (monthOptions = "July" && dayOptions <= 22) {
-		var signName = "Cancer";
-	} else if (monthOptions = "July" && dayOptions >= 23) {
-		var signName = "Leo";
-	} else if (monthOptions = "August" && dayOptions <= 22) {
-		var signName = "Leo";
-	} else if (monthOptions = "August" && dayOptions >= 23) {
-		var signName = "Virgo";
-	} else if (monthOptions = "September" && dayOptions <= 22) {
-		var signName = "Virgo";
-	} else if (monthOptions = "September" && 22 < dayOptions <= 30) {
-		var signName = "Libra";
-	} else if (monthOptions = "October" && dayOptions <= 22) {
-		var signName = "Libra";
-	} else if (monthOptions = "October" && dayOptions >= 23) {
-		var signName = "Scorpio";
-	} else if (monthOptions = "November" && dayOptions <= 21) {
-		var signName = "Scorpio";
-	} else if (monthOptions = "November" && 21 < dayOptions <= 30) {
-		var signName = "Sagittarius";
-	} else if (monthOptions = "December" && dayOptions <= 21) {
-		var signName = "Sagittarius";
-	} else if (monthOptions = "December" && dayOptions >= 22) {
-		var signName = "Capricorn";
-	} else if (monthOptions = "January" && dayOptions <= 19) {
-		var signName = "Capricorn";
-	} console.log(signName);
-}
+var zodiacChart = [
+	{
+		signValue: 11222,
+		signName: "Capricorn",
+		posTraits: "",
+		negTraits: "",
+		compatibleWith: ""
+	},
+	{
+		signValue: 11122,
+		signName: "Sagittarius",
+		posTraits: "",
+		negTraits: "",
+		compatibleWith: ""
+	},
+	{
+		signValue: 11023,
+		signName: "Scorpio",
+		posTraits: "",
+		negTraits: "",
+		compatibleWith: ""
+	},
+	{
+		signValue: 10923,
+		signName: "Libra",
+		posTraits: "",
+		negTraits: "",
+		compatibleWith: ""
+	},
+	{
+		signValue: 10823,
+		signName: "Virgo",
+		posTraits: "",
+		negTraits: "",
+		compatibleWith: ""
+	},
+	{
+		signValue: 10723,
+		signName: "Leo",
+		posTraits: "",
+		negTraits: "",
+		compatibleWith: ""
+	},
+	{
+		signValue: 10621,
+		signName: "Cancer",
+		posTraits: "",
+		negTraits: "",
+		compatibleWith: ""
+	},
+	{
+		signValue: 10521,
+		signName: "Gemini",
+		posTraits: "",
+		negTraits: "",
+		compatibleWith: ""
+	},
+	{
+		signValue: 10420,
+		signName: "Taurus",
+		posTraits: "",
+		negTraits: "",
+		compatibleWith: ""
+	},
+	{
+		signValue: 10321,
+		signName: "Aries",
+		posTraits: "",
+		negTraits: "",
+		compatibleWith: ""
+	},
+	{
+		signValue: 10219,
+		signName: "Pisces",
+		posTraits: "",
+		negTraits: "",
+		compatibleWith: ""
+	},
+	{
+		signValue: 10120,
+		signName: "Aquarius",
+		posTraits: "",
+		negTraits: "",
+		compatibleWith: ""
+	},
+	{
+		signValue: 10101,
+		signName: "Capricorn",
+		posTraits: "",
+		negTraits: "",
+		compatibleWith: ""
+	}
+];
+
+function signFinder(monthOption, dayOption) {
+	var x = document.getElementById("month");
+	var m = x.options[x.selectedIndex].value;
+	var y = document.getElementById("day");
+	var d = y.options[y.selectedIndex].value;
+	var value = parseInt(String(m) + String(d));
+
+	for (i = 0; i < zodiacChart.length; i++) {
+		if(value >= zodiacChart[i].signValue) {
+			console.log(zodiacChart[i].signName);
+			return value;
+		}
+	}
+};
 
 
 
